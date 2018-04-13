43c111d3f49e6be532b464a08f0074742a3deb0a
Lavrentiev Dima <lavrentiev.dima@mail.ru>
2018-04-13 11:02:14 +0300

    fix scroll bug on mobile version. optimize js show/hide aside bar.

--- app/js/common.js
+++ app/js/common.js
@@ -13,38 +13,53 @@ $(function () {
 
     $(document).on('scroll', function (e) {
         if ($(document).scrollTop() > 100) {
+            $('.always-visible-block-clone').removeClass('hide');
             $('.always-visible-block').addClass('sticky');
         }
         else {
+            $('.always-visible-block-clone').addClass('hide');
             $('.always-visible-block').removeClass('sticky');
         }
     });
 
     $("#btnToLeftBar").on('click', function () {
-        if($(".left-aside").attr('data-is_open') == 0) {
-            $(".left-aside").animate({
-                left: 0,
-            }, 500);
-            $(".left-aside").attr('data-is_open', 1);
-        } else {
-            $(".left-aside").animate({
-                left: '-300px',
-            }, 500);
-            $(".left-aside").attr('data-is_open', 0);
-        }
+        moveAsideBar($(".left-aside"), 'left');
+        // if($(".left-aside").attr('data-is_open') == 0) {
+        //     $(".left-aside").animate({
+        //         left: 0,
+        //     }, 500);
+        //     $(".left-aside").attr('data-is_open', 1);
+        // } else {
+        //     $(".left-aside").animate({
+        //         left: '-300px',
+        //     }, 500);
+        //     $(".left-aside").attr('data-is_open', 0);
+        // }
     });
     $("#btnToRightBar").on('click', function () {
-        if($(".right-aside").attr('data-is_open') == 0) {
-            $(".right-aside").animate({
-                right: 0,
-            }, 500);
-            $(".right-aside").attr('data-is_open', 1);
-        } else {
-            $(".right-aside").animate({
-                right: '-300px',
-            }, 500);
-            $(".right-aside").attr('data-is_open', 0);
-        }
+        moveAsideBar($(".right-aside"), 'right');
+        // if($(".right-aside").attr('data-is_open') == 0) {
+        //     $(".right-aside").animate({
+        //         right: 0,
+        //     }, 500);
+        //     $(".right-aside").attr('data-is_open', 1);
+        // } else {
+        //     $(".right-aside").animate({
+        //         right: '-300px',
+        //     }, 500);
+        //     $(".right-aside").attr('data-is_open', 0);
+        // }
     });
+
+    function moveAsideBar(jq_terger, direction) {
+        var options = {};
+        if(jq_terger.hasClass('closed')) {
+            options[direction] = 0;
+        } else {
+            options[direction] = '-300px';
+        }
+        jq_terger.animate(options, 500);
+        jq_terger.toggleClass('closed');
+    }
 
 });
